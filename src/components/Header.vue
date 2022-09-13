271123cef20060bf64e2d10013081856406ad027
Max <74653870+hesteren@users.noreply.github.com>
2022-09-13 18:34:20 +0200

    Added header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+</template>
+
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+
+<!-- scoped means only this component gets this stylesheet -->
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
